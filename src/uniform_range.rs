e17c7a3e1595e4e4ea4bb7bcfffcc19356e5ffde
Graham Dennis <graham.dennis@gmail.com>
2015-10-24 09:00:58 +1100

    Correct the implementation of distributions for uniform ranges.

--- src/uniform_range.rs
+++ src/uniform_range.rs
@@ -2,62 +2,77 @@ use core::{Distribution, IntoDistribution};
 use uniform::Uniform;
 
 use std::ops::{Range};
+use std::num::Wrapping as w;
 use rand::Rng;
-use num::{Integer, Bounded};
+use num::{Bounded};
 
-pub struct UniformIntegerRange<T> {
+pub struct UniformPrimitiveIntegerRange<T, TUnsigned> {
     low: T,
-    range: T,
-    accept_zone: T,
-    base_distribution: Uniform<T>
+    range: TUnsigned,
+    accept_zone: TUnsigned,
+    base_distribution: Uniform<TUnsigned>
 }
 
-impl <X: Integer + Bounded + Copy> UniformIntegerRange<X> where
-    Uniform<X>: Distribution<Output=X>
-{
-    #[inline]
-    pub fn new(low: X, high: X) -> UniformIntegerRange<X> {
-        assert!(low < high);
-        let range = high - low;
-        let max: X = Bounded::max_value();
-        let zone = max - (max % range);
+macro_rules! uniform_integer_range_impls {
+    ($(($ty:ty, $tyUnsigned:ty)),*) => {
+        $(
+            impl UniformPrimitiveIntegerRange<$ty, $tyUnsigned>
+            {
+                #[inline]
+                pub fn new(low: $ty, high: $ty) -> UniformPrimitiveIntegerRange<$ty, $tyUnsigned> {
+                    assert!(low < high);
+                    let unsigned_range = (w(high as $tyUnsigned) - w(low as $tyUnsigned)).0;
+                    let unsigned_max: $tyUnsigned = Bounded::max_value();
+                    let unsigned_zone = unsigned_max - (unsigned_max % unsigned_range);
 
-        UniformIntegerRange {
-            low: low,
-            range: range,
-            accept_zone: zone,
-            base_distribution: Uniform::new()
-        }
-    }
-}
-
-impl <X: Integer + Bounded + Copy> IntoDistribution<X> for Range<X> where
-    Uniform<X>: Distribution<Output=X>
-{
-    type Distribution = UniformIntegerRange<X>;
-
-    #[inline]
-    fn into_distribution(self) -> UniformIntegerRange<X> {
-        UniformIntegerRange::new(self.start, self.end)
-    }
-}
-
-impl <X: Integer + Copy> Distribution for UniformIntegerRange<X> where
-    Uniform<X>: Distribution<Output=X>
-{
-    type Output = X;
-
-    #[inline]
-    fn sample<R: Rng>(&self, rng: &mut R) -> X {
-        loop {
-            let v: X = self.base_distribution.sample(rng);
-
-            if v < self.accept_zone {
-                return self.low + (v % self.range);
+                    UniformPrimitiveIntegerRange {
+                        low: low,
+                        range: unsigned_range,
+                        accept_zone: unsigned_zone,
+                        base_distribution: Uniform::new()
+                    }
+                }
             }
-        }
+
+            impl IntoDistribution<$ty> for Range<$ty>
+            {
+                type Distribution = UniformPrimitiveIntegerRange<$ty, $tyUnsigned>;
+
+                #[inline]
+                fn into_distribution(self) -> UniformPrimitiveIntegerRange<$ty, $tyUnsigned> {
+                    UniformPrimitiveIntegerRange::<$ty, $tyUnsigned>::new(self.start, self.end)
+                }
+            }
+
+            impl Distribution for UniformPrimitiveIntegerRange<$ty, $tyUnsigned>
+            {
+                type Output = $ty;
+
+                #[inline]
+                fn sample<R: Rng>(&self, rng: &mut R) -> $ty {
+                    loop {
+                        let v: $tyUnsigned = self.base_distribution.sample(rng);
+
+                        if v < self.accept_zone {
+                            return (w(self.low) + w((v % self.range) as $ty)).0;
+                        }
+                    }
+                }
+            }
+        )*
     }
 }
+
+macro_rules! signed_and_unsigned_range_impls {
+    ($(($ty:ty, $tyUnsigned:ty)),*) => {
+        uniform_integer_range_impls! { $(($ty, $tyUnsigned), ($tyUnsigned, $tyUnsigned)),* }
+    }
+}
+
+signed_and_unsigned_range_impls! {
+    (i8, u8), (i16, u16), (i32, u32), (i64, u64), (isize, usize)
+}
+
 
 #[cfg(test)]
 mod tests {
